02d23c0434979ae881737540cc33fe534d6d9716
Farkhodjon <drnajimov@yahoo.com>
2024-01-26 09:45:58 -0500

    Add task solution

--- src/week01/task1/OddOrEven_Farhad.java
+++ src/week01/task1/OddOrEven_Farhad.java
@@ -1,4 +1,29 @@
 package week01.task1;
 
 public class OddOrEven_Farhad {
+
+    public static void main(String[] args) {
+        OddOrEven(5);
+        OddOrEven(6);
+    }
+
+    public static void OddOrEven(int n) {
+
+        if(n%2==0){
+            System.out.println("Even");
+        }else{
+            System.out.println("Odd");
+        }
+    }
+
+
 }
+
+/*
+  **Numbers -- odd & even**
+Write a method that can identify  a given number is even or odd.
+EX:
+       identify(5) --> "Odd"
+       identify(6) --> "Even"
+
+ */
--- src/week01/task3/FINRA_Farhad.java
+++ src/week01/task3/FINRA_Farhad.java
@@ -1,4 +1,30 @@
 package week01.task3;
 
 public class FINRA_Farhad {
+
+    public static void main(String[] args) {
+        FINRA(30);
+    }
+
+    public static void FINRA(int n){
+        for (int i = 1; i <= n; i++) {
+            if(i%3==0 && i%5==0){
+                System.out.println(i + " - FINRA");
+            }else if(i%3==0){
+                System.out.println(i + " - FIN");
+            } else if (i%5==0) {
+                System.out.println(i + " - RA");
+            }else System.out.println(i);;
+        }
+    }
+
+
 }
+
+/*
+**Numbers -- FINRA**
+Write a method that prints out the numbers from 1 to 30 but;
+for numbers which are multiple of 3 print "FIN" instead of the number,
+for numbers which are multiple of 5, print "RA" instead of the number,
+and for numbers which are multiple of both 3 and 5, print "FINRA" instead of the number.
+ */
