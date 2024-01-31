1abed29ffb17fc71beaf1029c18ab4e376ffd5bf
Guendouz27 <mohamedguendouz27@gmail.com>
2024-01-30 23:10:47 -0500

    task3 done

--- src/week01/task3/FINRA_Guendouz.java
+++ src/week01/task3/FINRA_Guendouz.java
@@ -1,4 +1,48 @@
 package week01.task3;
 
+import java.util.Scanner;
+
+
 public class FINRA_Guendouz {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int number = input.nextInt();
+
+
+
+        if (number %5 == 0 & number % 3 == 0){
+            System.out.println(" FINRA");
+        }
+
+
+
+        else if (number % 5 == 0){
+            System.out.println(" RA");
+        }
+        else if (number % 3 == 0) {
+                System.out.println(" FIN");
+            }
+
+
+        else { System.out.println(number);}
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
