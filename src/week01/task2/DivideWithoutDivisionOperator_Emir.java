4f09e71e26ca2cc712dbbdc0d0b406ddc91f6031
European01 <emiryaziciofficial@gmail.com>
2024-01-30 23:44:04 -0500

    Add more alternative solutions for DivideWithoutDivisionOperator task.

--- src/week01/task2/DivideWithoutDivisionOperator_Emir.java
+++ src/week01/task2/DivideWithoutDivisionOperator_Emir.java
@@ -8,8 +8,8 @@ public class DivideWithoutDivisionOperator_Emir {
         divideWithoutDivision(40,12);
         divideWithoutDivision(70,21);
         divideWithoutDivision(3,5);
-        //divideWithoutDivision(10,0);
         divideWithoutDivision(4,2);
+        //divideWithoutDivision(10,0);
 
         System.out.println("--------------------------------------------------");
 
@@ -24,8 +24,6 @@ public class DivideWithoutDivisionOperator_Emir {
         System.out.println("-----------------------------------------------------");
 
         try {
-           // int result1 = divideWithoutDivision2(300, 0);
-           // System.out.println("Result = " + result1);
 
             int result2 = division2(-30, -6);
             System.out.println("Result = " + result2);
@@ -35,6 +33,21 @@ public class DivideWithoutDivisionOperator_Emir {
 
             int result4 = division2(45, 9);
             System.out.println("Result = " + result4);
+
+            // int result1 = divideWithoutDivision2(300, 0);
+            // System.out.println("Result = " + result1);
+
+            int result6 = divideWithoutDivision2(15,3);
+            System.out.println("Result = " + result6);
+
+            int result7 = divideWithoutDivision2(60,12);
+            System.out.println("Result = " + result7);
+
+            int result8 = divideWithoutDivision2(20,7);
+            System.out.println("Result = " + result8);
+
+            //int result5 = divideWithoutDivision2(15,0);
+            //System.out.println("Result = " + result5);
 
         } catch (ArithmeticException e) {
             e.printStackTrace();
@@ -78,9 +91,10 @@ public class DivideWithoutDivisionOperator_Emir {
         }
     }
 
-    public static int divideWithoutDivision2(int a, int b)  {
+    public static int divideWithoutDivision2(int a, int b)  { // only works with positive integers
+
         if (b == 0) {
-            throw new ArithmeticException("Cannot divide by zero");
+            throw new ArithmeticException(a + " cannot divide by " + b);
         }
 
         int count = 0;
@@ -93,9 +107,10 @@ public class DivideWithoutDivisionOperator_Emir {
         return count;
     }
 
-    public static int division2(int a, int b)  {
+    public static int division2(int a, int b)  { // works with both positive and negative integers
+
         if (b == 0) {
-            throw new ArithmeticException("Cannot divide by zero");
+            throw new ArithmeticException(a + " cannot divide by " + b);
         }
 
         int sign = (a < 0) ^ (b < 0) ? -1 : 1; // Determine the sign of the result
