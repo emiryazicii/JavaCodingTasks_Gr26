d3a520e85e0a373fbac0b5324c9741d6cfeff648
jika17 <jikarai17@gmail.com>
2024-01-31 18:36:58 -0500

    updated 3 tasks_Jika

--- src/week01/task3/FINRA_Jika.java
+++ src/week01/task3/FINRA_Jika.java
@@ -1,4 +1,24 @@
 package week01.task3;
 
 public class FINRA_Jika {
+    public static void main(String[] args) {
+
+        displayFinra(30);
+
+    }
+     public static void displayFinra(int n){
+
+         for (int i = 1; i <= n; i++) {
+             if (i % 15 == 0){
+                 System.out.println("FINRA");
+             } else if (i % 3 == 0 ) {
+                 System.out.println("FIN");
+             }else if (i % 5 == 0){
+                 System.out.println("RA");
+             }else {
+                 System.out.println(i);
+             }
+
+         }
+     }
 }
