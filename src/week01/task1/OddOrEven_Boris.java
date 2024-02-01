59f037e82a5a53994a87830382cc2fcf7eb5f42b
Borislav <borkosudija@gmail.com>
2024-02-01 02:03:53 +0100

    add OddOrEven_Boris task codes

--- src/week01/task1/OddOrEven_Boris.java
+++ src/week01/task1/OddOrEven_Boris.java
@@ -1,4 +1,13 @@
 package week01.task1;
 
 public class OddOrEven_Boris {
+
+    public static void oddOrEven(int a) {
+
+        if (a % 2 == 0) {
+            System.out.println(a + " is even number");
+        } else {
+            System.out.println(a + " is odd number");
+        }
+    }
 }
