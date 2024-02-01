d6be1314a2778b25aa527db9619f8930564014c1
Borislav <borkosudija@gmail.com>
2024-02-01 01:59:10 +0100

    add DivideWithoutDivisionOperator_Boris task codes

--- src/week01/task2/DivideWithoutDivisionOperator_Boris.java
+++ src/week01/task2/DivideWithoutDivisionOperator_Boris.java
@@ -1,4 +1,20 @@
 package week01.task2;
 
 public class DivideWithoutDivisionOperator_Boris {
+
+    public static void divisionWithoutDivider(int a1, int a2){
+        if(a2 == 0){
+            System.err.println("Number can not be devided by 0");
+        }
+
+        int count = 0;
+
+        for (int i = 1; i < a2; i++) {
+            if(a1 >= a2){
+                count++;
+                a1-= a2;
+            }
+        }
+        System.out.println("Result is : " + count + " reminder " + a1);
+    }
 }
