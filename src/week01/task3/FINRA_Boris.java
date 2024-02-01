ea698c741109adbd7f388e972b62098633b375e9
Borislav <borkosudija@gmail.com>
2024-02-01 01:57:07 +0100

    add Finra task codes

--- src/week01/task3/FINRA_Boris.java
+++ src/week01/task3/FINRA_Boris.java
@@ -1,4 +1,21 @@
 package week01.task3;
 
 public class FINRA_Boris {
+
+    public static void finra(int num) {
+        int a = 1;
+        while (a <= num) {
+
+            if(a % 15 ==0){
+                System.out.println("FINRA");
+            }else if ( a % 5 == 0){
+                System.out.println("RA");
+            }else if (a % 3 == 0){
+                System.out.println("FIN");
+            }else {
+                System.out.println(a);
+            }
+            a++;
+        }
+    }
 }
