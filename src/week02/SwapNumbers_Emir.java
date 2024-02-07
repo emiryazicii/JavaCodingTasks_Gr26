4b56b5325dc8675e495ac7d8daa04fe76d779936
European01 <emiryaziciofficial@gmail.com>
2024-02-07 11:40:23 -0500

    Add my solution for the task.

--- src/week02/SwapNumbers_Emir.java
+++ src/week02/SwapNumbers_Emir.java
@@ -1,4 +1,22 @@
 package week02;
 
 public class SwapNumbers_Emir {
+
+    public static void main(String[] args) {
+
+        swapNumbers(15,10);
+        System.out.println("---------");
+        swapNumbers(5,10);
+
+    }
+
+    public static void swapNumbers(int a , int b){
+
+        a = a - b;
+        b = a + b ;
+        a = b - a ;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
 }
