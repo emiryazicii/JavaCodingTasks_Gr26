c11314279b75837b119bb006cf47fe4531d3ec76
bataborisa <borkosudija@gmail.com>
2024-02-14 18:35:07 +0100

    add ReverseNegativeNumber_Boris

---
+++ src/week03/task2/ReverseNegativeNumber_Boris.java
@@ -0,0 +1,27 @@
+package week03.task2;
+
+public class ReverseNegativeNumber {
+
+    public static void main(String[] args) {
+
+        System.out.println(reverseNegativeNumber(-53));
+    }
+
+
+    public static int reverseNegativeNumber(int a){
+
+        a = a * (-1);
+        int reversed = 0;
+
+     while (a > 0){
+         int lastDigit = a % 10;
+         reversed = reversed * 10 + lastDigit;
+         a /= 10;
+     }
+        return reversed * (-1);
+    }
+}
+/*
+Write a return method that can reverse digits of a negative number and return it as int
+input: -35  output: -53
+ */
