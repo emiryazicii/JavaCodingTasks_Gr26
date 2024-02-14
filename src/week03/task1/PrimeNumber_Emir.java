7f2fb5cc222d1770cf09558b5f54885c4aaa8057
European01 <emiryaziciofficial@gmail.com>
2024-02-14 09:41:53 -0500

    Add requirements.

--- src/week03/task1/PrimeNumber_Emir.java
+++ src/week03/task1/PrimeNumber_Emir.java
@@ -2,3 +2,6 @@ package week03.task1;
 
 public class PrimeNumber_Emir {
 }
+/*
+Write a method that can check if a number is prime or not
+ */
--- src/week03/task2/ReverseNegativeNumber_Emir.java
+++ src/week03/task2/ReverseNegativeNumber_Emir.java
@@ -2,3 +2,7 @@ package week03.task2;
 
 public class ReverseNegativeNumber_Emir {
 }
+/*
+Write a return method that can reverse digits of a negative number and return it as int
+input: -35  output: -53
+ */
