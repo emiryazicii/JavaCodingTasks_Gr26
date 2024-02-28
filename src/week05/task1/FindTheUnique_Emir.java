bdf6f75ae247d1b1c03060622aff1ea9d058c8ff
European01 <emiryaziciofficial@gmail.com>
2024-02-28 12:47:40 -0500

    Add requirements.

--- src/week05/task1/FindTheUnique_Emir.java
+++ src/week05/task1/FindTheUnique_Emir.java
@@ -2,3 +2,7 @@ package week05.task1;
 
 public class FindTheUnique_Emir {
 }
+/*
+Write a return method that can find the unique characters from the String
+ Ex: unique("AAABBBCCCDEF") ==> "DEF";
+ */
--- src/week05/task2/Reverse_Emir.java
+++ src/week05/task2/Reverse_Emir.java
@@ -2,3 +2,7 @@ package week05.task2;
 
 public class Reverse_Emir {
 }
+/*
+Write a function that can reverse a String
+ Ex: Reverse("ABCD"); ==> DCBA
+ */
