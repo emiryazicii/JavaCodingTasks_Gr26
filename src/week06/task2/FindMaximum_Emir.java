82f978f44dc5f1d6de581bfd90d04d08a9459c8b
European01 <emiryaziciofficial@gmail.com>
2024-03-06 10:27:14 -0500

    Add requirements.

--- src/week06/task2/FindMaximum_Emir.java
+++ src/week06/task2/FindMaximum_Emir.java
@@ -2,3 +2,7 @@ package week06.task2;
 
 public class FindMaximum_Emir {
 }
+/*
+Write a method that can find the maximum number from an int Array
+Ex:  int[] arr = {99, 12, 23, 32, 44, 57, 6} ==> 99
+ */
--- src/week06/task3/SumOfAllDigits_Emir.java
+++ src/week06/task3/SumOfAllDigits_Emir.java
@@ -2,3 +2,7 @@ package week06.task3;
 
 public class SumOfAllDigits_Emir {
 }
+/*
+Write a method that can return the sum of the digits in a String
+Ex:  "12 java 5 apple 3"  ==>  20
+ */
